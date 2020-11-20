1f731c1c11cae59435d03dd12990d7d8695ea0a1
Pu Du <pudugg@gmail.com>
2020-11-20 13:40:53 -0600

    add deck

---
+++ src/components/Deck.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Deck = () => {
+  return (
+    <div className="deck d-flex justify-content-center align-items-center">
+      <h1>Ready to start?</h1>
+    </div>
+  );
+};
+
+export default Deck;
