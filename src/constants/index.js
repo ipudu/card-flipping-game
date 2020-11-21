e632062c53d88cb0545b9e1b212db7df741e7541
Pu Du <pudugg@gmail.com>
2020-11-20 18:01:40 -0600

    add game constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,5 @@ export const CARD_CLICK = 'CARD_CLICK';
 export const CARD_MATCH = 'CARD_MATCH';
 export const GAME_START = 'GAME_START';
 export const GAME_RESET = 'GAME_RESET';
+export const GAME_WIN = 'GAME_WIN';
+export const GAME_TIME = 'GAME_TIME';
