5342675c00bd0a2332234b439fbb2b635d083efb
Pu Du <pudugg@gmail.com>
2020-11-20 21:19:40 -0600

    add card rest constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 export const CARD_CLICK = 'CARD_CLICK';
 export const CARD_MATCH = 'CARD_MATCH';
+export const CARD_RESET = 'CARD_RESET';
 export const GAME_START = 'GAME_START';
 export const GAME_RESET = 'GAME_RESET';
 export const GAME_WIN = 'GAME_WIN';
